fb94d1838c839e7e0559391653a450efe25c77d9
steampunkyi <u20219834@utp.edu.pe>
2020-04-06 21:32:16 -0500

    prueba2

--- string.py
+++ string.py
@@ -2,23 +2,25 @@ myStr = "Hello World"
 
 #print(dir(myStr))
 
-print(myStr.upper())
-print(myStr.lower())
-print(myStr.swapcase())
-print(myStr.capitalize())
-print(myStr.replace('Hello', 'bye'))
-print(myStr.count('o'))
+#print(myStr.upper())
+#print(myStr.lower())
+#print(myStr.swapcase())
+#print(myStr.capitalize())
+# print(myStr.replace('Hello', 'bye'))
+# print(myStr.count('o'))
 
-print(myStr.startswith('Hello'))
-print(myStr.endswith('ld'))
+# print(myStr.startswith('Hello'))
+# print(myStr.endswith('ld'))
 
-print(myStr.split())
-print(myStr.find('e'))
+# print(myStr.split())
+# print(myStr.find('e'))
 
-print(len(myStr))
-print(myStr.index('d'))
+# print(len(myStr))
+# print(myStr.index('d'))
 
-print(myStr.isnumeric())
-print(myStr.isalpha())
+# print(myStr.isnumeric())
+# print(myStr.isalpha())
 
-print(myStr[6])
+# print(myStr[6])
+# print(myStr[-2])
+
