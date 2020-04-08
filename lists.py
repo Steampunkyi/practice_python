9925f87c3b6500600163b39f818de866fba8bf30
steampunkyi <u20219834@utp.edu.pe>
2020-04-07 23:07:56 -0500

    prueba3

--- lists.py
+++ lists.py
@@ -2,7 +2,33 @@ demo_list = [1, 'hello', 1.34, True, [1, 2, 3]]
 colors = ['red', 'green', 'blue']
 
 number_list = list((1, 2, 3, 4))
-#print(type(number_list))
+# print(type(number_list))
 
-r = list(range(1, 10))
-print(r)
+# r = list(range(1, 100))
+# print(r)
+# print(len(demo_list))
+# print('green' in colors)
+# print(colors)
+# colors[1] = 'yellow'
+# print(colors)
+
+# colors.append('violet')
+# print(colors)
+# colors.extend(['violet', 'yellow'])
+# print(colors)
+
+# colors.insert(len(colors), 'violet')
+# print(colors)
+
+# colors.pop()
+# print(colors)
+
+# colors.remove('green')
+# print(colors)
+
+# colors.sort()
+# print(colors)
+# colors.sort(reverse=True)
+# print(colors)
+
+print(colors.index('blue'))
