a7fe5e4ffafd2629f2a6a1afd3ae71005653a55e
steampunkyi <u20219834@utp.edu.pe>
2022-01-26 10:03:42 -0500

    Agregado corazon

--- Excercices/set.py
+++ Excercices/set.py
@@ -1,5 +1,23 @@
-colors = {'Red', 'Green', 'Blue'}
-print(colors)
+#colors = {'Red', 'Green', 'Blue'}
+#print(colors)
 
-colors.add('Violet')
-print(colors)
+#colors.add('Violet')
+#print(colors)
+
+from turtle import *
+
+def curvemove():
+    for i in range(200):
+        right(1)
+        forward(1)
+
+color ('red', 'pink')
+begin_fill()
+left(140)
+forward(111.65)
+curvemove()
+left(120)
+curvemove()
+forward(111.65)
+end_fill()
+done()
