8eecaabab68072d40c518d370f7ea5f26ef0708b
steampunkyi <u20219834@utp.edu.pe>
2022-03-18 15:11:49 -0500

    Ejercicios de Python

--- Excercices/Persona.py
+++ Excercices/Persona.py
@@ -1,8 +1,8 @@
 class Persona:
     def __init__(self, nombre, apellido, edad):
         self._nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
+        self._apellido = apellido
+        self._edad = edad
 
     @property
     def nombre(self):
@@ -12,9 +12,31 @@ class Persona:
     def nombre(self, nombre):
         self._nombre = nombre
 
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+
+    @property
+    def edad(self):
+        return self._edad
+
+    @edad.setter
+    def edad(self, edad):
+        self._edad = edad
+
     def mostrar_detalle(self):
-        print(f'Persona: {self._nombre} {self.apellido} {self.edad}')
+        print(f'Persona: {self._nombre} {self._apellido} {self._edad}')
 
+    def __del__(self):
+        print(f'Persona: {self.nombre} {self.apellido}')
 
-persona1 = Persona('Juan', 'Perez', 30)
-persona1.mostrar_detalle()
+if __name__ == '__main__':
+    persona1 = Persona('Juan', 'Perez', 30)
+    persona1.nombre = 'Juan Carlos'
+    persona1.apellido = 'Lara'
+    persona1.edad = 40
+    persona1.mostrar_detalle()
---
+++ Herencia/ClientePersona.py
@@ -0,0 +1,7 @@
+from Persona import *
+
+persona1 = Persona("Juan", 25)
+print(persona1)
+
+empleado1 = Empleado("Pedro", 30, 5000)
+print(empleado1)
